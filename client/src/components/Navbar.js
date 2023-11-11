0aa6d2d2fdbdfc632bcace9141c5e2c43e3f0825
daus <rivakanzf@gmail.com>
2023-11-11 22:57:37 +0700

    product page

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"></img>
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+            </a>
+            <div className="items-center" id="navbar-sticky">
+                <form>   
+                    <div className="relative">
+                        <input type="search" id="default-search" className="block w-96 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari parfum kesukaanmu" required></input>
+                        <button type="submit" className="text-white absolute end-2.5 bottom-2.5  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                            </svg>
+                        </button>
+                    </div>
+                </form>
+            </div>
+            <div className="flex ">
+                <button type="button" className=" mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
+                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Daftar</button>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,50 +1,48 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar'
 import Product from '../components/Product'
+import Footer from '../components/Footer'
 
 const Landing = () => {
   return (
     <>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-            <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="https://flowbite.com/docs/images/logo.svg" className="h-8" alt="Flowbite Logo"></img>
-                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                </a>
-                <div className="items-center" id="navbar-sticky">
-                    <form>   
-                        <div className="relative">
-                            <input type="search" id="default-search" className="block w-96 p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Cari parfum kesukaanmu" required></input>
-                            <button type="submit" className="text-white absolute end-2.5 bottom-2.5  hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                                </svg>
-                            </button>
-                        </div>
-                    </form>
-                </div>
-                <div className="flex ">
-                    <button type="button" className=" mr-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Masuk</button>
-                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Daftar</button>
-                </div>
-            </div>
-        </nav>
+        <Navbar></Navbar>
         {/* product list */}
-        <div className='mt-32 max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <div className='mt-32 max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-4'>
             <div className='newest-newest-product'>
-                <h1 className='font-bold text-2xl'>Terbaru</h1>
-                <div className='flex'>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
-                    <Product/>
+                <h1 className='font-bold text-2xl text-left ml-10'>Terbaru</h1>
+                <div className='flex my-4'>
+                    <button type='button'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M15.293 3.293 6.586 12l8.707 8.707 1.414-1.414L9.414 12l7.293-7.293-1.414-1.414z"/></svg></button>
+                    <div className='flex mx-4 w-full'>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                    </div>
+                    <button type='button'><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="M7.293 4.707 14.586 12l-7.293 7.293 1.414 1.414L17.414 12 8.707 3.293 7.293 4.707z"/></svg></button>
                 </div>
             </div>
             <div className='available-product'>
-
+                <h1 className='font-bold text-2xl text-left ml-10'>Produk Tersedia</h1>
+                <div className='flex my-4 ml-6'>
+                    <div className='flex flex-wrap mx-4 w-full'>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                        <Product/>
+                    </div>
+                </div>
+                <button type="button" className="m-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Lihat lebih banyak</button>
             </div>
         </div>
+        {/* footer */}
+        <Footer></Footer>
     </>
   )
 }
