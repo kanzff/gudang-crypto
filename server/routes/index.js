ac9858ec93c952d4b2810638ed4a6239f9bf7ffa
daus <rivakanzf@gmail.com>
2023-11-17 23:08:45 +0700

    tes vercel

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const router = require('express').Router()
 const productRoutes = require('./product')
 const userRoutes = require('./user')
 
+router.get('/', (req, res) => {
+    res.send("its working")
+})
 router.use('/product', productRoutes)
 router.use('/user', userRoutes)
 
